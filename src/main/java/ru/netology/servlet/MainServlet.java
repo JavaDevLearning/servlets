1ce01d7f638d600baa90be4ad1c24ae9ae8aafb1
Andrey Belkin <dkvd1@yandex.ru>
2023-06-23 08:26:48 +0500

    Di-Java

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -12,54 +12,55 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
-  public static void main(String[] args) {
-    final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
-    final var controller = context.getBean("postController");
-  }
-
-  private static final String METHOD_GET = "GET";
-  private static final String METHOD_POST = "POST";
-  private static final String METHOD_DELETE = "DELETE";
-  private static final String PATH_POSTS = "/api/posts";
-
-  private PostController controller;
-
-
-  @Override
-  protected void service(HttpServletRequest req, HttpServletResponse resp) {
-    // если деплоились в root context, то достаточно этого
-    try {
-      final var path = req.getRequestURI();
-      final var method = req.getMethod();
-
-      // primitive routing
-      if (method.equals(METHOD_GET) && path.equals(PATH_POSTS)) {
-        controller.all(resp);
-        return;
-      }
-      if (method.equals(METHOD_GET) && path.matches(PATH_POSTS + "/\\d+")) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
-        controller.getById(id, resp);
-        return;
-      }
-      if (method.equals(METHOD_POST) && path.equals(PATH_POSTS)) {
-        controller.save(req.getReader(), resp);
-        return;
-      }
-      if (method.equals(METHOD_DELETE) && path.matches(PATH_POSTS + "/\\d+")) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
-        controller.removeById(id, resp);
-        return;
-      }
-      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    } catch (NotFoundException e) {
-      e.printStackTrace();
-      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    } catch (Exception e) {
-      e.printStackTrace();
-      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    @Override
+    public void init() {
+        final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+        final var controller = context.getBean("postController");
     }
-  }
+
+    private static final String METHOD_GET = "GET";
+    private static final String METHOD_POST = "POST";
+    private static final String METHOD_DELETE = "DELETE";
+    private static final String PATH_POSTS = "/api/posts";
+
+    private PostController controller;
+
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // если деплоились в root context, то достаточно этого
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+
+            // primitive routing
+            if (method.equals(METHOD_GET) && path.equals(PATH_POSTS)) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals(METHOD_GET) && path.matches(PATH_POSTS + "/\\d+")) {
+                // easy way
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals(METHOD_POST) && path.equals(PATH_POSTS)) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals(METHOD_DELETE) && path.matches(PATH_POSTS + "/\\d+")) {
+                // easy way
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
